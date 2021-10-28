895986a907abd9a422146adb871ab1c16d6f7d81
Joao Gomes <joaopfg.2002@gmail.com>
2021-10-28 19:50:56 +0100

    Initial structure

---
+++ Building.cs
@@ -0,0 +1,12 @@
+﻿namespace Code
+{
+    public class Building
+    {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public int FloorsNumber { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Controllers/BuildingController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Code.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BuildingController : ControllerBase
+    {
+
+
+        private readonly ILogger<BuildingController> _logger;
+
+        public BuildingController(ILogger<BuildingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Building> Get()
+        {
+
+            var buildings = new List<Building>();
+
+            buildings.Add(new Building
+            {
+                Name = "The Shard",
+                Street = "London Bridge St",
+                Town = "London",
+                Postcode = "SE1 9SG",
+                FloorsNumber = 87,
+                Country = "United Kingdom"
+
+            });
+
+            buildings.Add(new Building
+            {
+                Name = "One Canada Square",
+                Street = "Canary Wharf",
+                Town = "London",
+                Postcode = "E14 5AB",
+                FloorsNumber = 50,
+                Country = "United Kingdom"
+
+            });
+
+            return buildings;
+        }
+    }
+}
