b0a311684871b292eee26c044a097e42c81a33b9
João <1200363@isep.ipp.pt>
2021-11-04 22:18:19 +0000

    Feat: CRUD Flat

--- Controllers/FlatController.cs
+++ Controllers/FlatController.cs
@@ -32,5 +32,67 @@ namespace Code.Controllers
 
             });
         }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Flat>), 200)]
+        [ProducesResponseType(500)]
+        public IEnumerable<Flat> Get()
+        {
+            return flats;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id){
+             Flat flat = flats.Find(p => p.Id == id);
+
+            if(flats == null)
+                return NotFound();
+
+            return Ok(flats) ;
+        }
+
+        [HttpPost]
+        public ActionResult Create(Flat flat){
+
+            flats.Add(flat);
+
+            return CreatedAtAction(
+                nameof(Create), 
+                new {
+                id = flat.Id
+                },
+                flat
+            );
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, Flat flat){
+            if(id != flat.Id){
+                return NotFound();
+            }
+
+            var existingFlatIndex = flats.FindIndex(p => p.Id == flat.Id);
+
+            if(existingFlatIndex == -1){
+                return StatusCode(400);
+            }
+
+           flats[existingFlatIndex] = flat;
+
+           return StatusCode(200);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id){
+             Flat existingFlat = flats.Find(p => p.Id == id);
+
+            if(existingFlat is null){
+                return NotFound();
+            }
+            
+             flats.Remove(existingFlat);
+
+             return StatusCode(200);
+        }
     }
 }
