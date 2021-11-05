eab609ba11500e8695641a17e9c249a74443507c
Dayana Moreira <1191809@isep.ipp.pt>
2021-11-05 17:50:16 +0000

    Create Model/Owner & Cotroller/Owner

---
+++ Models/Owner.cs
@@ -0,0 +1,10 @@
+namespace Code
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
