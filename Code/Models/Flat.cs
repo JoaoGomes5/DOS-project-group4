adc50e3215400e4e7c387d997e2d148887f74c6d
Joao Gomes <joaopfg.2002@gmail.com>
2021-11-18 00:29:46 +0000

    Create itShouldBeAbleToCalculateFlatCost Test

--- Code/Models/Flat.cs
+++ Code/Models/Flat.cs
@@ -3,7 +3,7 @@ namespace Code.Models
     public class Flat
     {
         public int Id { get; set; }
-        public int SquareMeters { get; set; }
+        public float SquareMeters { get; set; }
         public int NumberOfRooms { get; set; }
 
         public int OwnerId { get; set;}
