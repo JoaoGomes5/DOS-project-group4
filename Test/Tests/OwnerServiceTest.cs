250d35378fba91fb304b82ffbf9eadcd3771a09c
Joao Gomes <joaopfg.2002@gmail.com>
2021-11-18 00:28:52 +0000

    Create itShouldBeAbleToCalculateFlatCost Test

--- Test/Tests/OwnerServiceTest.cs
+++ Test/Tests/OwnerServiceTest.cs
@@ -1,16 +1,19 @@
 using NUnit.Framework;
 using Code.Models;
+using Code.Services;
 
 namespace Test
 {
-    public class Tests
+    public class OwnerServiceTest
     {
+        
+              
+        
        
         [Test]
         public void itShouldBeAbleToCalculateOwnerFlatsCost()
         {
-
-            Assert.Pass();
+            
         }
     }
 }
